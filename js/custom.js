31d24f51bcf727fa7e2a39cda8856f19b2acce62
ejioforthadeus <ejioforthaddeus037@gmail.com>
2022-10-19 17:36:20 +0100

    for a fontional website

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(function () {
 
 	setTimeout(function () {
 		$('.loader_bg').fadeToggle();
-	}, 9500);
+	}, 1500);
 
 	/* JQuery Menu
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
